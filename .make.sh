454a625f48dd8427fdf2353907346b91696393ea
Keith <k3ithk@gmail.com>
2016-03-16 17:43:29 -0500

    Adds powerline fonts install on os x and fixes indent

--- .make.sh
+++ .make.sh
@@ -24,15 +24,26 @@ echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file $olddir
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+	echo "Moving any existing dotfiles from ~ to $olddir"
+	mv ~/.$file $olddir
+	echo "Creating symlink to $file in home directory."
+	ln -s $dir/$file ~/.$file
 done
 
 #install vim-plug if it isn't there already
 if [ ! -d ~/.vim/autoload/plug.vim ]; then
 	curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+# install powerline fonts if on os x
+if [[ 'Darwin' == `uname` ]]; then
+	git clone https://github.com/powerline/fonts.git
+	cd fonts
+	./install.sh
+	cd ..
+	rm -rf fonts/
+else
+	echo "You must manually install the fonts if not on OS X"
 fi
 
 # Install those plugins
